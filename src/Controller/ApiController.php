dcb85525292640b6e6633f8e133ed3b80043d434
DiegoMazenc <contact@diego-mazenc.com>
2024-04-19 11:48:24 +0200

    réparation du secure

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -7,7 +7,7 @@ use App\Entity\Hall;
 use App\Repository\BandRepository;
 use App\Repository\HallRepository;
 use App\Repository\BandMemberRepository;
-use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
